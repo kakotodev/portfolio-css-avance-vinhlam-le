bc3fb37cd9a6c3ad657262f8085c7a42a516bfae
kakotodev <vinh-lam.le@efrei.net>
2025-03-28 18:01:03 +0100

    Finalisation du projet pour le devoir de CSS Avancé

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("button_presentation").addEventListener("click", function() {
+    document.getElementById("sub-1").scrollIntoView();
+})
+
+document.getElementById("button_competence").addEventListener("click", function() {
+    document.getElementById("sub-2").scrollIntoView();
+})
+
+document.getElementById("button_experience").addEventListener("click", function() {
+    document.getElementById("sub-3").scrollIntoView();
+})
